f1c2d9cdf8deaafaf59cbece63ab30a31562f61e
feruza22 <feruza2218@gmail.com>
2023-08-14 08:47:39 -0700

    teskari

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+for(var i = 0;i<=10;i++){
+console.log(i);
+}
+  
+var bosh=[];
+var arr =[0, 1 ,2 ,3 ,4 ,5 ,6, 7, 8, 9, 10];
+for(var i=10;i>0;i--){
+    bosh.push(arr[i]);
+}
+console.log(bosh);
